89e3c4c2f5d3819c1116b7e1791988f9cc59c809
Karthik1192 <mrkarthik1992@gmail.com>
2017-03-07 19:26:13 +0530

    Add files via upload
    Second Commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: true,
+
+    region: 'us-east-1',
+
+    identityPoolId: 'us-east-1:634225213090:userpool/us-east-1_xtc3EsHML',
+    userPoolId: 'us-east-1_xtc3EsHM',
+    clientId: '2sbcba292sjkgmjo1c6j2o2gai',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-1',
+
+    ddbTableName: 'LoginTrail'
+};
